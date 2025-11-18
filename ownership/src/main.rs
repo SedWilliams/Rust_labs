695c1b94d50cbfc5275a935b27cc531fb41ae0a2
tre3iii <lilshed3@gmail.com>
2025-11-17 23:31:29 -0800

    update

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,24 +1,45 @@
 fn main() {
 
+
+    //Ownership is applied to heap allocated variables (mutable variables,custom types,inputs,etc)
+    //CORRECTION: Only used with types that do not implement the Copy trait
+    
+    //Stack allocated variables do not have standard ownership rules
+    //simply not true
+
+    //Types that do not have the copy type operate under standard ownership
+    
+    //Functions can be owners
+
     /***************************
-     * Rust 'Ownership' Rules 
+     * Copy types
+     ***************************/
+    let x: i32 = 10;
+    //let y: u8 = x; invalid because literals are copied although not bound by regular ownership
+    
+    //works because the copied value is of the same type
+    let y: i32 = x;
+
+    //works because regular ownership rules are loopholed here
+    println!("{}", x);
+
+    /***************************
+     * Non-copy types
      ***************************/
 
-    //1. Each piece of data in Rust has an 'Owner', a reference to a memory location (a variable)
-    //   is an owner
-    //2. Each memory location can only have one Owner at a time.
-    //
-    //3. When the owner goes out of scope, the memory is dropped automatically.
+    //becomes invalid after y is initialized
+    let x = String::from("Hello World");
+    let y = x;
 
-
-    /***************************
-     * Scope
-     ***************************/
-    // Global scope (project scope)
-    // File scope
-    // Function scope
-    // Block scope
-    // Expression scope
-
-
+    //produces error because the heap allocated x is dropped after ownership is moved to y
+    //println!("{}", x);
+    
+    
+    /*
+     * Copy types are standard literals like &str, int, float, boolean
+     *
+     * Non-copy type examples: pointers, String, structs
+     *
+     * Check the rust documentation for what types implement the copy trait.
+     */
 }
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/home/shedrick/dev/learn-rust/ownership/target/debug/ownership: /home/shedrick/dev/learn-rust/ownership/src/main.rs
