9e4f32420b3df863b6de59f46071ac01730d92fa
ShedsDesktop <lilshed3@gmail.com>
2025-11-22 18:38:35 -0800

    upd

---
+++ IO_test/target/debug/IO_test.d
@@ -0,0 +1,1 @@
+D:\dev\rust\learn-rust\IO_test\target\debug\IO_test.exe: D:\dev\rust\learn-rust\IO_test\src\main.rs
---
+++ IO_test/target/debug/deps/IO_test.d
@@ -0,0 +1,5 @@
+D:\dev\rust\learn-rust\IO_test\target\debug\deps\IO_test.exe: src\main.rs
+
+D:\dev\rust\learn-rust\IO_test\target\debug\deps\IO_test.d: src\main.rs
+
+src\main.rs:
